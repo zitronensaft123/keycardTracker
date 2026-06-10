02c4d15d2f82b843d1e7c634894fab1632513b15
lolo <lorenz.schroepf@htl-braunua.at>
2026-06-10 17:39:12 +0200

    Database Creation

--- db.py
+++ db.py
@@ -7,8 +7,28 @@ def initDB():
     cursor = conn.cursor()
 
     cursor.execute('''
-                   CREATE TABLE IF NOT EXISTS items (
-                   itemID INTEGER PRIMARY KEY NOT NULL,
+                   CREATE TABLE IF NOT EXISTS items
+                   (
+                   itemID  PRIMARY KEY NOT NULL,
                    name TEXT NOT NULL,
-                   price INTEGER)
+                   price INTEGER
+                   )
+                   ''')
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS raids
+                   (
+                   raidID INTEGER PRIMARY KEY AUTOINCREMENT,
+                   date TEXT NOT NULL,
+                   )
+                   ''')
+    cursor.execute('''
+                   CREATE TABLE IF NOT EXISTS foundItems
+                   (
+                   raid_ID INTEGER NOT NULL,
+                   itemID   INTEGER NOT NULL,
+                   quantity INTEGER,
+                   PRIMARY KEY(raidID, itemID),
+                   FOREIGN KEY(raidID) REFERENCES raids(raidID)
+                   FOREIGN KEY(itemID) REFERENCES items(itemID)
+                   )
                    ''')
